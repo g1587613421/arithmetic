268125f69856f08fa8ca7684eac773f89f32039b
高金磊 <g1587613421@outlook.com>
2020-01-21 13:36:53 +0800

    数组嵌套
    执行用时 :
    1606 ms
    , 在所有 Java 提交中击败了
    5.16%
    的用户
    内存消耗 :
    61.2 MB
    , 在所有 Java 提交中击败了
    5.26%
    的用户

---
+++ src/领扣算法/AA中等题/数组嵌套/Main.java
@@ -0,0 +1,26 @@
+package 领扣算法.AA中等题.数组嵌套;
+
+public class Main {
+    boolean[] flag;
+    int max=0;
+    public int arrayNesting(int[] nums) {
+        flag=new boolean[nums.length];
+//        maxs=new int[nums.length];
+        int len;
+        for (int i = 0; i < nums.length; i++) {
+            len=1;
+            int target=nums[i];
+            while (target!=i){
+                target=nums[target];
+                len++;
+                if (target>=nums.length){
+                    len=0;
+                    break;
+                }
+            }
+            max=Math.max(len, max);
+        }
+
+        return max;
+    }
+}
