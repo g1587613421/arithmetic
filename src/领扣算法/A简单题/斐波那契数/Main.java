6e1040419057834c16e9d3aeed43264e850cf60f
高金磊 <g1587613421@outlook.com>
2020-02-23 20:33:43 +0800

    执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 36 MB , 在所有 Java 提交中击败了 5.11% 的用户

--- src/领扣算法/A简单题/斐波那契数/Main.java
+++ src/领扣算法/A简单题/斐波那契数/Main.java
@@ -5,10 +5,22 @@
 package 领扣算法.A简单题.斐波那契数;
 
 public class Main {
-//暴力
+    int fir=0,sec=1,middle;
     public int fib(int N) {
         if (N<2)
-            return N-1;
-        return fib(N-1)+fib(N-2);
+            return N;
+        for (int i = 0; i < N; i++) {
+            middle=sec;
+            sec+=fir;
+            fir=middle;
+        }
+        return sec;
     }
+
+////暴力
+//    public int fib(int N) {
+//        if (N<2)
+//            return N-1;
+//        return fib(N-1)+fib(N-2);
+//    }
 }
