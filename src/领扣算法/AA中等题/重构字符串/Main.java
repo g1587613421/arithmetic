f46d2873de2c7c53b96404f4e0a58cfa47cd2137
高金磊 <g1587613421@outlook.com>
2020-02-25 08:13:25 +0800

    执行用时 : 6 ms , 在所有 Java 提交中击败了 28.41% 的用户 内存消耗 : 37.4 MB , 在所有 Java 提交中击败了 5.09% 的用户

---
+++ src/领扣算法/AA中等题/重构字符串/Main.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.AA中等题.重构字符串;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {    HashMap<Character,Integer> data=new HashMap<>();
+    public String reorganizeString(String S) {
+        for (char c : S.toCharArray()) {
+            data.put(c, data.getOrDefault(c, 0)+1);
+        }
+        int da[][]=new int[data.size()][2];
+        int i=0;
+        for (Map.Entry<Character, Integer> entry : data.entrySet()) {
+            da[i++]=new int[]{entry.getKey(),entry.getValue()};
+        }
+        if (da.length<2)
+            return "";
+        StringBuffer stringBuffer=new StringBuffer();
+        sortint(da);
+        while (da[0][1]!=0){
+            stringBuffer.append((char)da[0][0]);
+            da[0][1]--;
+            if (da[1][1]==0){
+                sortint(da);
+                break;
+            }
+            stringBuffer.append((char)da[1][0]);
+            da[1][1]--;
+            sortint(da);
+        }
+        if (da[0][1]!=0)
+            return "";
+        return stringBuffer.toString();
+
+
+    }
+    private void sortint(int[][] da){
+        Arrays.sort(da,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[1]-o1[1];
+            }
+        });
+    }
+
+    
+    public static void main(String arg[]){//main--来自高金磊
+    
+        new Main().reorganizeString("aab");
+    }
+}
