00614d1b0097bc38094d6522de06009249e953b9
高金磊 <g1587613421@outlook.com>
2020-02-27 17:28:29 +0800

    aaa

--- src/蓝桥题库/算法训练/景点游览/Main.java
+++ src/蓝桥题库/算法训练/景点游览/Main.java
@@ -4,5 +4,27 @@
 
 package 蓝桥题库.算法训练.景点游览;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String arg[]){//main--来自高金磊
+
+         Scanner scanner=new Scanner(System.in);
+         Integer nums[]=new Integer[scanner.nextInt()];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=scanner.nextInt();
+        }
+        Arrays.sort(nums,new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        for (Integer num : nums) {
+            System.out.print(num+" ");
+        }
+    }
 }
