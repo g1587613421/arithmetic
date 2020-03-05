c55b542ef51c306ea324a9c07bc8c53a9c876517
高金磊 <g1587613421@outlook.com>
2020-03-05 19:40:01 +0800

    执行用时 : 1 ms , 在所有 Java 提交中击败了 28.89% 的用户 内存消耗 : 37.7 MB , 在所有 Java 提交中击败了 5.18% 的用户

--- src/领扣算法/A简单题/一周中的第几天/Main.java
+++ src/领扣算法/A简单题/一周中的第几天/Main.java
@@ -4,49 +4,14 @@
 
 package 领扣算法.A简单题.一周中的第几天;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
+import java.time.LocalDate;
 public class Main {
 
-    public String dayOfTheWeek(int day, int month, int year) {
-        String[] res=new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        Date date=new Date(year,month,day);
-        Calendar calendar=Calendar.getInstance();
-        calendar.setTime(date);
-
-        return res[calendar.get(Calendar.DAY_OF_WEEK)];
-    }
-    public static String weekByDate (int year,int month,int day)
-    {
-        String str="";
-        SimpleDateFormat fmt = new SimpleDateFormat("dd MM yyyy");
-
-        Date d = null;
-        try {
-            d = fmt.parse(day+" "+month+" "+year);
-        }catch (Exception e)      {
-            e.printStackTrace();
-    }
-
-        Calendar cal = Calendar.getInstance();
-
-        cal.setTime(d);
-
-        int weekDay = cal.get(Calendar.DAY_OF_WEEK);
-
-        switch(weekDay) {
-            case 1 : str="SUNDAY"; break;
-            case 2 : str="MONDAY"; break;
-            case 3 : str="TUESDAY"; break;
-            case 4 : str="WEDNESDAY"; break;
-            case 5 : str="THURSDAY"; break;
-            case 6 : str="FRIDAY"; break;
-            case 7 : str="SATURDAY"; break;
-            default: break;
+        public  String dayOfTheWeek (int day, int month, int year)
+        {
+            LocalDate localDate=LocalDate.of(year, month, day);
+            int da = localDate.getDayOfWeek().getValue();
+            return new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}[da%7];
         }
 
-        return str;
-    }
 }
