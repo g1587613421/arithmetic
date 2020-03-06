955d270a49784de3f51fd6c511f1d35bcbd5237e
高金磊 <g1587613421@outlook.com>
2020-03-06 16:44:25 +0800

    执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 36.8 MB , 在所有 Java 提交中击败了 5.30% 的用户

---
+++ src/领扣算法/A简单题/转换成小写字母/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.A简单题.转换成小写字母;
+
+public class Main {
+
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+
+    }
+}
