3b006080b284ff5058bed32bf2099afebe9bad2a
高金磊 <g1587613421@outlook.com>
2020-03-07 15:39:55 +0800

    执行用时 : 46 ms , 在所有 Java 提交中击败了 47.42% 的用户 内存消耗 : 52.5 MB , 在所有 Java 提交中击败了 98.47% 的用户

--- src/领扣算法/A简单题/最近的请求次数/RecentCounter.java
+++ src/领扣算法/A简单题/最近的请求次数/RecentCounter.java
@@ -7,22 +7,19 @@ package 领扣算法.A简单题.最近的请求次数;
 import java.util.ArrayList;
 
 public class RecentCounter {
-    ArrayList<Integer> data,middle;
+    ArrayList<Integer> data;
 
     public RecentCounter() {
         data = new ArrayList<>();
-        middle=new ArrayList<>();
+
     }
 
     public int ping(int t) {
-        middle.clear();
         data.add(t);
-        for (Integer datum : data) {
-            if (t-datum>3000){
-                middle.add(datum);
-            }
+        while (t-data.get(0)>3000){
+            data.remove(0);
         }
-        data.removeAll(middle);
+
         return data.size();
     }
 }
