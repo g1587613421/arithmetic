d32dbfa74bdf6ca679c0e60e9d690f02074e529e
高金磊 <g1587613421@outlook.com>
2020-03-09 20:21:21 +0800

    执行用时 : 4 ms , 在所有 Java 提交中击败了 95.87% 的用户 内存消耗 : 42.1 MB , 在所有 Java 提交中击败了 100.00% 的用户

--- src/Tools/ShowDataState.java
+++ src/Tools/ShowDataState.java
@@ -10,9 +10,18 @@ public class ShowDataState<T> {
             System.out.print(""+0+"\t");
         }
     }
+
     public void showArrays(T[][] o){
         for (T[] ts : o) {
             for (T t : ts) {
+                System.out.print(""+t+"\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void showInt_Arrays(int[][] o){
+        for (int[] ts : o) {
+            for (int t : ts) {
                 System.out.print(""+t+"\t");
             }
             System.out.println();
--- src/领扣算法/AA中等题/矩阵区域和/Main.java
+++ src/领扣算法/AA中等题/矩阵区域和/Main.java
@@ -8,8 +8,36 @@ public class Main {
 
     public int[][] matrixBlockSum(int[][] mat, int K) {
         //构造二维最小前缀和
+        int[][] middle = new int[mat.length + 1][mat[0].length + 1];
+        middle[1][1]=mat[0][0];
+        for (int i = 1; i < mat.length+1; i++) {
+            middle[i][1]=middle[i-1][1]+mat[i-1][0];
+        }
+        for (int i = 1; i <mat[0].length+1; i++) {
+            middle[1][i]=middle[1][i-1]+mat[0][i-1];
+        }
+        for (int i = 1; i <mat.length+1 ; i++) {
+            for (int j = 1; j < mat[0].length+1; j++) {
+                middle[i][j]=middle[i-1][j]+middle[i][j-1]-middle[i-1][j-1]+mat[i-1][j-1];
+            }
+        }
+        int[][] res = new int[mat.length][mat[0].length];
+        int x,y,w,z;
+        for (int i = 0; i < mat.length; i++) {
+            x=i+K+1;
+            x=(x>=middle.length)?middle.length-1:x;
+            z=i-K;
+            z=z<0?0:z;
+            for (int j = 0; j < mat[0].length; j++) {
+                y=j+K+1;
+                y=y>middle[0].length-1?middle[0].length-1:y;
+                w=j-K;
+                w=w<0?0:w;
+                res[i][j]=middle[x][y]-middle[x][w]-middle[z][y]+middle[z][w];
+            }
 
+        }
 
-        return null;
+        return res;
     }
 }
