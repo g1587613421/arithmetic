43dd6cec1ad20d5f520efe121ba06c3a65195ed0
高金磊 <g1587613421@outlook.com>
2020-03-09 22:40:03 +0800

    执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 37.5 MB , 在所有 Java 提交中击败了 5.38% 的用户

---
+++ src/领扣算法/A简单题/链表的中间结点/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.A简单题.链表的中间结点;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+class Main {
+    public ListNode middleNode(ListNode head) {
+        ListNode middle=head;
+        while (head.next!=null){
+            middle=middle.next;
+            head=head.next;
+            if (head.next!=null)
+                head=head.next;
+        }
+        return middle;
+    }
+}
