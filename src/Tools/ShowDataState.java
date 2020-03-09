8981220e5bb3b5c541ed9721d8cfe56dcdb7a47c
高金磊 <g1587613421@outlook.com>
2020-03-09 18:12:28 +0800

    新增数组打印器

---
+++ src/Tools/ShowDataState.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package Tools;
+
+public class ShowDataState<T> {
+    public void showArray(T[] o){
+        for (T t : o) {
+            System.out.print(""+0+"\t");
+        }
+    }
+    public void showArrays(T[][] o){
+        for (T[] ts : o) {
+            for (T t : ts) {
+                System.out.print(""+t+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
