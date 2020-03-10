d344a6ca42a9d8abc3dad6bf88e086fc00f0de73
高金磊 <g1587613421@outlook.com>
2020-03-11 07:09:50 +0800

    执行用时 : 2 ms , 在所有 Java 提交中击败了 86.81% 的用户 内存消耗 : 43.6 MB , 在所有 Java 提交中击败了 100.00% 的用户

---
+++ src/领扣算法/A简单题/将数组分成和相等的三个部分/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.A简单题.将数组分成和相等的三个部分;
+
+public class Main {
+
+    public boolean canThreePartsEqualSum(int[] A) {
+        int target=0;
+        int[] middle = new int[A.length + 1];
+        middle[0]=0;
+        for (int i = 0; i < A.length; i++) {
+            middle[i+1]=middle[i]+A[i];
+        }
+        if (middle[middle.length-1]%3!=0)
+            return false;
+        target=middle[middle.length-1]/3;
+        int start=0,end=1,count=0;
+        while (end<middle.length){
+            if (middle[end]-middle[start]==target)
+            {
+                count++;
+                start=end;
+            }
+         end++;
+
+        }
+        return count==3||count>3&&target==0;
+    }
+}
