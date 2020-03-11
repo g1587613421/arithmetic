1caf9a6669ddf32f55958b5e477f798694f0f541
高金磊 <g1587613421@outlook.com>
2020-03-11 19:36:04 +0800

    执行用时 : 1 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 39.1 MB , 在所有 Java 提交中击败了 5.03% 的用户

---
+++ src/领扣算法/AA中等题/TinyURL的加密与解密/Codec.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.AA中等题.TinyURL的加密与解密;
+
+public class Codec {
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+
+        return shortUrl;
+    }
+}
