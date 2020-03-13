afa8c3620bbe44b8c37eafe1816fd3e09a7361c8
高金磊 <g1587613421@outlook.com>
2020-03-13 08:33:26 +0800

    执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 42.6 MB , 在所有 Java 提交中击败了 5.12% 的用户

---
+++ src/领扣算法/AA中等题/寻找重复数/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.AA中等题.寻找重复数;
+
+public class Main {
+
+    public int findDuplicate(int[] nums) {
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        int ptr1 = nums[0];
+        int ptr2 = tortoise;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+}
