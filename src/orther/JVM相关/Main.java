6c6fb862d3c4c8dad02d81e367fd77efec2bba2c
高金磊 <g1587613421@outlook.com>
2020-03-19 10:29:02 +0800

    JVM相关

---
+++ src/orther/JVM相关/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package orther.JVM相关;
+
+public class Main {
+
+    public static void main(String[] arg){//main--来自高金磊
+        Integer age = 10;
+        Integer age2 = 10;
+        Integer age3 = 133;
+        Integer age4 = 133;
+        System.out.println(age==age2);
+        System.out.println(age3==age4);
+    }
+}
