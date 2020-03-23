114f39366e8d10937fbcf67f36f9a5b147c78d39
高金磊 <g1587613421@outlook.com>
2020-03-23 19:09:47 +0800

    ---条件概率 执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 36.8 MB , 在所有 Java 提交中击败了 5.13% 的用户

---
+++ src/领扣算法/AA中等题/飞机座位分配概率/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.AA中等题.飞机座位分配概率;
+
+public class Main {
+    public double nthPersonGetsNthSeat(int n) {
+        return n==1?1.0:0.5;
+    }
+}
