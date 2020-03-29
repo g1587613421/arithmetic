2bcdcf4491e33c52eea887bf845a6a573d4ad0a3
高金磊 <g1587613421@outlook.com>
2020-03-29 10:21:05 +0800

    执行用时 : 9 ms , 在所有 Java 提交中击败了 86.90% 的用户 内存消耗 : 43.5 MB , 在所有 Java 提交中击败了 5.34% 的用户

--- src/领扣算法/AA中等题/最长公共子序列/Main.java
+++ src/领扣算法/AA中等题/最长公共子序列/Main.java
@@ -7,25 +7,17 @@ package 领扣算法.AA中等题.最长公共子序列;
 public class Main {
 
     public int longestCommonSubsequence(String text1, String text2) {
-
-        if (text1.length()>text2.length())
-        {
-            String middle=text1;
-            text1=text2;
-            text2=middle;
-        }
-        int m=0,n=1;
-        int max=0;
-        while (n<=text1.length()){
-            if (text2.contains(text1.substring(m, n))){
-                max=n-m;
-                n++;
-            }
-            else {
-                n++;
-                m++;
+        char[] ts=text1.toCharArray();
+        char[] ts2=text2.toCharArray();
+        int[][] data = new int[ts.length + 1][ts2.length + 1];
+        for (int i = 1; i <data.length ; i++) {
+            for (int j = 1; j < data[0].length; j++) {
+                if (ts[i-1]==ts2[j-1])
+                    data[i][j]=data[i-1][j-1]+1;
+                else
+                    data[i][j]=Math.max(data[i][j-1],data[i-1][j]);
             }
         }
-        return max;
+        return data[data.length-1][data[0].length-1];
     }
 }
