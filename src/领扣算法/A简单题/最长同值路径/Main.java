234b14b8aaed5c951b748f48c4ec6fbd649cb85d
高金磊 <g1587613421@outlook.com>
2020-03-30 12:47:11 +0800

    执行用时 : 116 ms , 在所有 Java 提交中击败了 5.81% 的用户 内存消耗 : 53.7 MB , 在所有 Java 提交中击败了 5.03% 的用户

--- src/领扣算法/A简单题/最长同值路径/Main.java
+++ src/领扣算法/A简单题/最长同值路径/Main.java
@@ -15,28 +15,60 @@ class TreeNode {
       }
 
     class Solution {
-     int max=0;
-     LinkedList<TreeNode> data=new LinkedList<>();
+
+    LinkedList<TreeNode> data=new LinkedList<>();
         public int longestUnivaluePath(TreeNode root) {
+            if (root==null)
+                return 0;
             data.add(root);
-            while (data.size()!=0){
-                dp(data.poll(), 1);
-            }
-            return max;
-        }
-        private int dp(TreeNode treeNode,int de){
-            if (treeNode.right==treeNode.left)
+            int res=0;
+            while (!data.isEmpty())
             {
-                max=Math.max(max, de);
-                return de+1;
+                System.out.println(data.peek().val);
+                res=Math.max(res, plus(data.peekFirst().left, data.peekFirst().val)+(plus(data.peekFirst().right, data.pollFirst().val)));
+
             }
-            if (treeNode.left!=null&&treeNode.left.val==treeNode.val){
-                de=dp(treeNode.left, de+1);
-            }
-            if (treeNode.right!=null&&treeNode.right.val==treeNode.val){
-                de=dp(treeNode, de+1);
-            }
-            return de;
+          return res;
         }
+        private int plus(TreeNode treeNode,int target){
+            if (treeNode==null||target!=treeNode.val) {
+                if (treeNode!=null)
+                    data.addLast(treeNode);
+                return 0;
+            }
+            int sum=1;
+            int le=plus(treeNode.left, target);
+            int ri= plus(treeNode.right, target);
+            if (le!=0&&ri!=0)
+                data.addLast(treeNode);
+
+           sum+=Math.max(le,ri);
+           return sum;
+        }
+
+
+//     int max=0;
+//     LinkedList<TreeNode> data=new LinkedList<>();
+//        public int longestUnivaluePath(TreeNode root) {
+//            data.add(root);
+//            while (data.size()!=0){
+//                dp(data.poll(), 1);
+//            }
+//            return max;
+//        }
+//        private int dp(TreeNode treeNode,int de){
+//            if (treeNode.right==treeNode.left)
+//            {
+//                max=Math.max(max, de);
+//                return de+1;
+//            }
+//            if (treeNode.left!=null&&treeNode.left.val==treeNode.val){
+//                de=dp(treeNode.left, de+1);
+//            }
+//            if (treeNode.right!=null&&treeNode.right.val==treeNode.val){
+//                de=dp(treeNode, de+1);
+//            }
+//            return de;
+//        }
 
     }
