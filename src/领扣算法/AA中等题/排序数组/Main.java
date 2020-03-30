7ef2f179924178e2b55fbb337c78282c55d34922
高金磊 <g1587613421@outlook.com>
2020-03-31 07:28:49 +0800

    执行用时 : 4 ms , 在所有 Java 提交中击败了 99.66% 的用户 内存消耗 : 47.7 MB , 在所有 Java 提交中击败了 5.74% 的用户

---
+++ src/领扣算法/AA中等题/排序数组/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.AA中等题.排序数组;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
