705c0814def91d322ef2a66db48ce6b6ce3c97a7
高金磊 <g1587613421@outlook.com>
2020-04-07 14:48:48 +0800

    1.8方法糖

---
+++ src/orther/JVM相关/语法糖.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package orther.JVM相关;
+
+public class 语法糖 {
+
+
+    public static void main(String arg[]){//main--来自高金磊
+        int[] nums={1,2,3};
+    }
+}
