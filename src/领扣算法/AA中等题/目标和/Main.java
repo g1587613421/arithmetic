76cca1901d08c5339fd4ece66552350c374736a8
高金磊 <g1587613421@outlook.com>
2020-04-09 12:35:42 +0800

    执行用时 : 777 ms , 在所有 Java 提交中击败了 11.56% 的用户 内存消耗 : 37.4 MB , 在所有 Java 提交中击败了 5.64% 的用户

---
+++ src/领扣算法/AA中等题/目标和/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.AA中等题.目标和;
+
+public class Main {
+
+
+    public int findTargetSumWays(int[] nums, int S) {
+
+    }
+
+
+
+
+//    int count=0;
+//    public int findTargetSumWays(int[] nums, int S) {
+//        plus(nums, S, 0);
+//        return count;
+//    }
+//    void plus(int[] nums,int target,int index){
+//        if (index==nums.length)
+//        {
+//            count+=target==0?1:0;
+//            return;
+//        }
+//        plus(nums, target-nums[index],index+1);
+//        plus(nums, target+nums[index],index+1);
+//    }
+}
