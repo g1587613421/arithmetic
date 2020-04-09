99da6a29b687be235f74083f97b25eab55dfe122
高金磊 <g1587613421@outlook.com>
2020-04-09 20:48:07 +0800

    执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 37.2 MB , 在所有 Java 提交中击败了 33.33% 的用户

---
+++ src/领扣算法/AA中等题/困于环中的机器人/Main.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020.版权所有高金磊
+ */
+
+package 领扣算法.AA中等题.困于环中的机器人;
+
+public class Main {
+    public boolean isRobotBounded(String instructions) {
+        int x=0;
+        int y=0;
+        int dirction=0;
+        for (char c : instructions.toCharArray()) {
+            switch (c) {
+                case 'L':
+                    dirction--;
+                    break;
+                case 'R':
+                    dirction++;
+                    break;
+                case 'G':
+                    dirction=dirction<0?dirction+4:dirction;
+                    switch (dirction % 4) {
+                        case 0:
+                            x++;
+                            break;
+                        case 1:
+                            y++;
+                            break;
+                        case 2:
+                            x--;
+                            break;
+                        case 3:
+                            y--;
+                            break;
+
+                    }
+
+            }
+
+        }
+        return (x==0&&y==0)||dirction%4!=0;
+    }
+    
+    
+    public static void main(String arg[]){//main--来自高金磊
+    
+       new Main().isRobotBounded("GLRLLGLL");
+    }
+}
