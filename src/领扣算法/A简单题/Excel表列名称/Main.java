f15c39396f6da0099afe2f4d15921450c4eee7b4
高金磊 <g1587613421@outlook.com>
2020-05-06 18:31:21 +0800

    执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗 : 36.5 MB , 在所有 Java 提交中击败了 14.29% 的用户

--- src/领扣算法/A简单题/Excel表列名称/Main.java
+++ src/领扣算法/A简单题/Excel表列名称/Main.java
@@ -5,4 +5,19 @@
 package 领扣算法.A简单题.Excel表列名称;
 
 public class Main {
+    public String convertToTitle(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (n != 0) {
+            n --;//这里稍作处理，因为它是从1开始
+            stringBuilder.append((char)(n % 26 + 'A'));
+            n /= 26;
+        }
+        return stringBuilder.reverse().toString();
+    }
+    public static void main(String arg[]){//main--来自高金磊
+    
+        new Main().convertToTitle(111);
+    }
 }
+
+
