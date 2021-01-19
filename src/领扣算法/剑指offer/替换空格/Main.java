c88191eb3382029e270cc153babcf880429318a3
高金磊 <g1587613421@outlook.com>
2021-01-19 11:31:34 +0800

    offer

---
+++ src/领扣算法/剑指offer/替换空格/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021.版权所有高金磊
+ */
+
+package 领扣算法.剑指offer.替换空格;
+
+public class Main {
+    public String replaceSpace(String s) {
+        return s.replaceAll(" ", "%20");
+    }
+}
