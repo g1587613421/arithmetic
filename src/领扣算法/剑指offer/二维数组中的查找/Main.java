a3929656213362d5be92f395daefcb161cd7e48f
高金磊 <g1587613421@outlook.com>
2021-01-25 09:20:32 +0800

    offer---timeerr

---
+++ src/领扣算法/剑指offer/二维数组中的查找/Main.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021.版权所有高金磊
+ */
+
+package 领扣算法.剑指offer.二维数组中的查找;
+
+public class Main {
+
+
+
+
+
+
+
+//    int data[][];
+//    boolean flag=false;
+//    int target=0;
+//    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+//        if (matrix.length==0)
+//            return false;
+//        data=matrix;//破坏性读取
+//        this.target=target;
+//        return plus_X(0, 0);
+//    }
+//
+//    //并查集
+//    boolean plus_X(int i,int j){
+//        if (flag)//提前终结信号
+//            return flag;
+//        if (i>=data.length||j>=data[0].length)
+//            return false;
+//        if (data[i][j]==target)
+//       {
+//           flag=true;
+//           return true;
+//        }
+//        if (data[i][j]>target)
+//            return false;
+//        return plus_X(i+1, j)||plus_X(i, j+1);
+//
+//    }
+
+}
