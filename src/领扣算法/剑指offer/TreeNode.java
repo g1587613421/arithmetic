43887ae3e390940e022f20ef0ed7ae38a3302670
高金磊 <g1587613421@outlook.com>
2021-01-26 08:31:13 +0800

    offer

---
+++ src/领扣算法/剑指offer/TreeNode.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021.版权所有高金磊
+ */
+
+package 领扣算法.剑指offer;
+
+ class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+ }
