0a6a71d4dcd4857e3eb4c6ab025e509a610c710c
高金磊 <g1587613421@outlook.com>
2021-03-10 15:29:50 +0800

    复试

---
+++ src/复试必胜/往年试题/出栈序列/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021.版权所有高金磊
+ */
+
+package 复试必胜.往年试题.出栈序列;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String arg[]){//main--来自高金磊
+
+        LinkedList<Integer> data=new LinkedList<>();
+         Scanner scanner=new Scanner(System.in);
+         int sum=scanner.nextInt();
+         //最多有一次排序方式发生改变
+        //不容易实现
+
+
+        //模拟
+        int nums[]=new int[sum];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=Integer.valueOf(Integer.valueOf(scanner.next()));
+        }
+        int n=1;
+        for (int i = 0; i < nums.length; i++) {
+            while (data.isEmpty()||data.peekLast()!=nums[i])
+            {
+                data.addLast(n);
+                if (n>sum){
+                    System.out.println(false);
+                    return;
+                }
+                n++;
+            }
+            data.pollLast();
+        }
+        System.out.println(data.isEmpty());
+
+    }
+
+}
