fa9a01691b5832d665fbbaeb74e48883872031eb
高金磊 <g1587613421@outlook.com>
2021-03-30 09:45:42 +0800

    力扣

--- src/领扣算法/A简单题/字符串中不同整数的数目/Main.java
+++ src/领扣算法/A简单题/字符串中不同整数的数目/Main.java
@@ -4,5 +4,30 @@
 
 package 领扣算法.A简单题.字符串中不同整数的数目;
 
+import java.util.HashSet;
+
 public class Main {
+    public int numDifferentIntegers(String word) {
+        char[] data=word.toCharArray();
+        for (int i = 0; i < data.length; i++) {
+            if (data[i]<'0'||data[i]>'9'){
+                data[i]=' ';
+            }
+        }
+        word=new String(data);
+        while (word.contains("  ")){
+            word=word.replaceAll("  "," ");
+        }
+        HashSet<String> res=new HashSet<>();
+        for (String s : word.split(" ")) {
+            if (!s.equals(""))
+                res.add(plus(s));
+        }
+        return res.size();
+    }
+    private String plus(String s){
+        while (s.startsWith("0"))
+            s=s.replaceFirst("0", "");
+        return s;
+    }
 }
